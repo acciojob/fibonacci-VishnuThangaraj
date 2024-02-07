6ff425b76266f37606364de62ad43fc446ad0772
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2024-02-07 21:17:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
 // your code here
+	let n1 = 0,n2 =1 ,n3=0;
+
+	  for(let i=1;i<n;i++){
+		  n3=n1+n2;
+		  n1=n2;
+		  n2=n3;
+	  }
+return n1;
 }
 
 module.exports = fibonacci;
